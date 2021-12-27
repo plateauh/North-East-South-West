28a5a85f8f1fd390ffa6052b95f056d27cc05634
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-27 15:11:02 +0300

    The question is: why unwind segue? what's the purpose here?

--- NorthEastSouthWest/DirectionsViewController.swift
+++ NorthEastSouthWest/DirectionsViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class DirectionsViewController: UIViewController {
 
+    
+    @IBAction func directionButtonPressed(_ sender: UIButton) {
+        directionPressed = sender.titleLabel?.text
+    }
+    
+    var directionPressed: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! SingleDirectionViewController
+        destination.direction = directionPressed
+    }
 
 }
 
--- NorthEastSouthWest/SingleDirectionViewController.swift
+++ NorthEastSouthWest/SingleDirectionViewController.swift
@@ -9,21 +9,12 @@ import UIKit
 
 class SingleDirectionViewController: UIViewController {
 
+    @IBOutlet weak var directionLabel: UILabel!
+    var direction: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        directionLabel.text = direction
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
