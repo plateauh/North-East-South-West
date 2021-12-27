ed119ee7e0479fa2f39a23b4e07af95e4afc37b2
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-27 14:48:09 +0300

    My constraints suck as always

---
+++ NorthEastSouthWest/SingleDirectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SingleDirectionViewController.swift
+//  NorthEastSouthWest
+//
+//  Created by Najd Alsughaiyer on 27/12/2021.
+//
+
+import UIKit
+
+class SingleDirectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
